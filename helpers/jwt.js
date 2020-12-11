0767612d0228cffd8940ab268c164b884782b88c
bobbyseptianto <bobbyseptianto@gmail.com>
2020-12-12 00:34:17 +0700

    Rev JWT

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 function signToken(payload) {
-  const token = jwt.sign(payload, process.env.SECRET);
+  const token = jwt.sign(payload, "SECRET");
   return token;
 }
 
 function verifyToken(access_token) {
-  const decoded = jwt.verify(access_token, process.env.SECRET);
+  const decoded = jwt.verify(access_token, "SECRET");
   return decoded;
 }
 
