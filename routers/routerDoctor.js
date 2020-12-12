4f6519e9111e4291c527953fc39d63063b58a31b
ams0 <adly.ms@gmail.com>
2020-12-12 09:06:52 +0700

    get detail doctor controller, routes add authentication, test

--- routers/routerDoctor.js
+++ routers/routerDoctor.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const DoctorController = require("../controllers/DoctorController");
+const authentication = require('../middlewares/authentication')
 
 router.post("/", DoctorController.loginDoctor);
+
+router.use(authentication)
+router.get('/detail', DoctorController.detailDoctor)
 // dst
 
 module.exports = router;
