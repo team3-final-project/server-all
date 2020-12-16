b69a52702ec381a878cbd76ea5b677198e97151d
dickyandeyan <dickyundead@gmail.com>
2020-12-16 14:26:54 +0700

    minor

--- controllers/PatientController.js
+++ controllers/PatientController.js
@@ -1,44 +1,43 @@
-const { Patient, HospitalRecord, MedicalRecord } = require("../models/index");
-const { signToken } = require("../helpers/jwt");
+const { Patient, HospitalRecord, MedicalRecord } = require('../models/index')
+const { signToken } = require('../helpers/jwt')
 
 class PatientController {
   static async loginPatient(req, res, next) {
     try {
-      const data = req.body;
+      const data = req.body
       const result = await Patient.findOne({
         where: {
           nik: data.nik,
-          name: data.name,
-        },
-      });
-
+          name: data.name
+        }
+      })
       if (!result) {
-        throw { msg: "Check again your identification data", status: 401 };
+        throw { msg: 'Check again your identification data', status: 401 }
       } else {
         const access_token = signToken({
           id: result.id,
           nik: result.nik,
-          name: result.name,
-        });
-        res.status(200).json({ access_token });
+          name: result.name
+        })
+        res.status(200).json({ access_token })
       }
     } catch (err) {
-      next(err);
+      next(err)
     }
   }
 
   static async readPatient(req, res, next) {
     try {
-      const patient = req.patientLoggedIn;
+      const patient = req.patientLoggedIn
       const patientData = await Patient.findOne({
         where: { nik: patient.nik },
-        include: [HospitalRecord, MedicalRecord],
-      });
-      res.status(200).json({ patientData });
+        include: [HospitalRecord, MedicalRecord]
+      })
+      res.status(200).json({ patientData })
     } catch (err) {
-      next(err);
+      next(err)
     }
   }
 }
 
-module.exports = PatientController;
+module.exports = PatientController
